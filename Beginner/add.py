0a4fad14cd785a6aa8c1a9e04dd9f51e9f4ca74d
Maogasteve <stevemagare4@gmail.com>
2022-10-11 11:18:17 +0300

    Snowballing effect

--- Beginner/add.py
+++ Beginner/add.py
@@ -1,7 +1,27 @@
-a = int(input("Please enter the first number:"))
+'''a = int(input("Please enter the first number:"))
 b = int (input("Enter the second number:"))
 s = a + b 
 print("The final result is:",s)
 age = 6000
 txt =  "years old since it was created by Jesus Christ" + age 
-print(txt)
+print(txt)'''
+
+# another add solution
+"""x = input("Please enter an integer:")
+y = input("please enter an integer:")
+sum  = int(x) +int(y)
+print("The sum is : ", sum)"""
+
+# How toReverse a string 
+"""txt = "Lord when" [::-1]
+print(txt)"""
+#Function 
+"""def my_function(x):
+    return x[::-1]
+mytext = my_function("am perplexed how this text looks like backwards")
+print(mytext)"""
+
+# How to remove duplicates from the list
+mylist = ["steve","steve", "apple","yea"]
+mylist = list(dict.fromkeys(mylist))
+print(mylist)
---
+++ Beginner/compiler.py
@@ -0,0 +1,5 @@
+print ("Steve was sufferring from a malady on 7th October 2022 ")
+x = "Coding "
+y = "is"
+z = "Spectaculars"
+print (x,y,z)
